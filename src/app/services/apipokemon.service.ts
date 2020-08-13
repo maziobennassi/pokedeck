081eb1a28f82f508aa338f0adf3ef41e046d41cd
Mazio Bennassi De Souza <mbsouza1@latam.stefanini.com>
2020-08-13 11:56:27 -0300

    Refatoração no service

--- src/app/services/apipokemon.service.ts
+++ src/app/services/apipokemon.service.ts
@@ -18,32 +18,25 @@ export class ApiPokemonService {
   constructor(private http: HttpClient) { }
 
   buscarTodasCartas(): Observable<ResponseCarta> {
-    const API_URL = this.baseUrlConsultar;
-    return this.get(API_URL);
+    return this.get();
   }
 
   buscarTodasCartasPaginado(pagina: number, tamanhoPagina: number): Observable<ResponseCarta> {
-    const API_URL = this.baseUrlConsultar;
     const API_QUERY = `?page=${pagina}&pageSize=${tamanhoPagina}`;
-    return this.get(`${API_URL}${API_QUERY}`);
+    return this.get(API_QUERY);
   }
 
   buscarTodasCartasPaginadoPorNome(pagina: number, tamanhoPagina: number, filtro: string): Observable<ResponseCarta> {
-    const API_URL = this.baseUrlConsultar;
     const API_QUERY = `?page=${pagina}&pageSize=${tamanhoPagina}&name=${filtro}`;
-    return this.get(`${API_URL}${API_QUERY}`);
+    return this.get(API_QUERY);
   }
 
-  buscarCarta(id: string): Observable<ResponseCarta> {
-    const API_URL = `${this.baseUrlConsultar}/${id}`;
-    return this.get(API_URL);
+  private get(API_QUERY: string = ""): Observable<ResponseCarta> {
+    const API_URL = this.baseUrlConsultar;
+    return this.http.get<ResponseCarta>(`${API_URL}${API_QUERY}`).pipe(catchError(this.handleError));
   }
 
-  protected get(endpoint: string): Observable<ResponseCarta> {
-    return this.http.get<ResponseCarta>(endpoint).pipe(catchError(this.handleError));
-  }
-
-  protected handleError(error: Response | any) {
+  private handleError(error: Response | any) {
     return Observable.throw(error);
   }
 }
